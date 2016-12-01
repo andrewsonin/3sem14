ea8a4ebc501b3cf27efddbee9ee3f333025dca39
Andrew Sonin <sonin.cel@gmail.com>
2016-12-01 16:05:50 +0300

    Add files via upload

--- bubble_sort.py
+++ bubble_sort.py
@@ -1,8 +1,8 @@
 def bubble(B):
-    A, k = B[0:], 0
+    A, s = B[0:], 0
     for k in range(1, len(A)):
         for i in range(len(A)-k):
             if A[i] > A[i+1]:
                 A[i], A[i+1] = A[i+1], A[i]
-                k += 1
-    return k
+                s += 1
+    return s
