14c3ee10cbbfc7966b8224b70707b4ddd37bca37
Andrew Sonin <sonin.cel@gmail.com>
2016-12-01 16:31:20 +0300

    Delete hanoi.py

--- hanoi.py
+++
@@ -1,9 +0,0 @@
-def hanoi(n, i=1, k=2):
-    if n == 1:
-        print(n, i, k)
-    else:
-        tmp = 6 - i - k
-        hanoi(n-1, i, tmp)
-        print(n, i, k)
-        hanoi(n-1, tmp, k)
-hanoi(10)
