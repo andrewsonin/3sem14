df545a59c7c3a8ffbde84228512cd198157ec78a
Andrew Sonin <sonin.cel@gmail.com>
2016-12-01 18:37:13 +0300

    Add files via upload

--- generation.py
+++ generation.py
@@ -1,8 +1,8 @@
 def generation(cash=5, people=1, m=1):
     if people == 0:
         return 0
-    quantity = cash // 5
-    for i in range(quantity + 1):
+    coins = cash // 5
+    for i in range(coins + 1):
         m += generation(cash - i, people - 1)
     return m
 print(generation(15, 2))
