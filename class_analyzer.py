816c4b2af989352307e4adeb7623026ede6ba5ba
Andrew Sonin <sonin.cel@gmail.com>
2016-12-08 01:07:05 +0300

    Add files via upload

---
+++ class_analyzer.py
@@ -0,0 +1,38 @@
+def push(A, data):
+    p = A
+    A = [data, p]
+
+
+class LinkedList:
+    def __init__(self):
+        self._begin = None
+
+    def push_front(self, data):
+        p = self._begin
+        self._begin = [data, p]
+
+    def pop_front(self):
+        if not self._begin:
+            raise IndexError
+        data = self._begin[0]
+        self._begin = self._begin[1]
+        return data
+
+    def empty(self):
+        return not self._begin
+
+
+def correct_braces_expression(sequence):
+    left, right = ['(', '['], [')', ']']
+    pair, stack = dict(zip(left, right)), LinkedList()
+    for brace in sequence:
+        if brace in left:
+            stack.push_front(brace)
+        elif brace in right:
+            if stack.empty():
+                return False
+            if pair[stack.pop_front()] != brace:
+                return False
+    return stack.empty()
+
+print(correct_braces_expression('((()))'))
